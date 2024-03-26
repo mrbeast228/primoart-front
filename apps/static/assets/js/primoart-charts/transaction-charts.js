4dca448f069145bb0b6b4e1a54388edeaf4dc456
Maksim Demichev <mdemv@ya.ru>
2024-03-26 11:58:06 +0300

    Заполнена плашка по выполнению транзакций

--- apps/static/assets/js/primoart-charts/transaction-charts.js
+++ apps/static/assets/js/primoart-charts/transaction-charts.js
@@ -19,36 +19,33 @@ function rand(min, max) {
 
 transactioncharts = {
 
-    initFloatingChart: function() {
+    initPerformanceChart: function() {
 
-        const labels = ["DNS Lookup", "Connection", "SSL Handshake", "First Byte", "Content Transfer"]
+        const labels = ["18.03.2024", "19.03.2024", "20.03.2024", "21.03.2024", "22.03.2024", "23.03.2024", "24.03.2024", "25.03.2024"];
 
         const data = {
           labels: labels,
           datasets: [
             {
-              label: 'Время выполнения шагов транзакции, с',
-              data: [
-                [0, 0.45],
-                [0.45, 0.59],
-                [0.59, 0.81],
-                [0.81, 1.07],
-                [1.07, 1.18]
-              ],
-                backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(255, 159, 64, 0.2)',
-                  'rgba(255, 205, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(54, 162, 235, 0.2)'
-                ],
-                borderColor: [
-                  'rgb(255, 99, 132)',
-                  'rgb(255, 159, 64)',
-                  'rgb(255, 205, 86)',
-                  'rgb(75, 192, 192)',
-                  'rgb(54, 162, 235)'
-                ]
+              label: 'Среднее время выполнения транзакций, с',
+              data: [13.1, 15.2, 9.5, 7.8, 18.9, 3.1, 2.7, 11.4],
+                backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                borderColor: 'rgb(255, 99, 132)',
+                yAxisID: 'y'
+            },
+            {
+              label: 'Количество транзакций за сутки',
+              data: [1285, 1175, 2357, 1245, 1624, 476, 328, 1134],
+                backgroundColor: 'rgba(255, 159, 64, 0.2)',
+                borderColor: 'rgb(255, 159, 64)',
+                yAxisID: 'y1'
+            },
+            {
+              label: 'Количество ошибок выполнения',
+              data: [125, 208, 56, 178, 73, 12, 43, 250],
+                backgroundColor: 'rgba(75, 192, 192, 0.2)',
+                borderColor: 'rgb(75, 192, 192)',
+                yAxisID: 'y1'
             }
           ]
         };
@@ -56,23 +53,42 @@ transactioncharts = {
         console.log(data);
 
         const config = {
-          type: 'bar',
+          type: 'line',
           data: data,
           options: {
             responsive: true,
+            interaction: {
+              mode: 'index',
+              intersect: false,
+            },
+            stacked: false,
             plugins: {
-              legend: {
-                position: 'top',
-              },
               title: {
                 display: false,
-                text: ''
+                text: 'Chart.js Line Chart - Multi Axis'
+              }
+            },
+            scales: {
+              y: {
+                type: 'linear',
+                display: true,
+                position: 'left',
+              },
+              y1: {
+                type: 'linear',
+                display: true,
+                position: 'right',
+
+                // grid line settings
+                grid: {
+                  drawOnChartArea: false, // only want the grid lines for one axis to show up
+                },
               }
             }
-          }
+          },
         };
 
-        var ctx = document.getElementById("chartTransactionFloationg01").getContext("2d");
+        var ctx = document.getElementById("chartTransactionPerf01").getContext("2d");
         var floatingChart = new Chart(ctx, config);
     }
 }
