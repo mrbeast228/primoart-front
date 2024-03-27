71b56a737064452fb0d4129b36c3caf2e090ba08
Maksim Demichev <mdemv@ya.ru>
2024-03-27 09:48:20 +0300

    Добавлен флоатинг чарт по шагам

--- apps/static/assets/js/primoart-charts/sandbox-transaction-charts.js
+++ apps/static/assets/js/primoart-charts/sandbox-transaction-charts.js
@@ -35,6 +35,66 @@ function generateData() {
   return data;
 }
 
+steprunchart = {
+
+    init: function() {
+
+        const labels = ["DNS Lookup", "Connection", "SSL Handshake", "First Byte", "Content Transfer"]
+
+        const data = {
+          labels: labels,
+          datasets: [
+            {
+              label: 'Время выполнения шагов транзакции, с',
+              data: [
+                [0, 0.45],
+                [0.45, 0.59],
+                [0.59, 0.81],
+                [0.81, 1.07],
+                [1.07, 1.18]
+              ],
+                backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(255, 159, 64, 0.2)',
+                  'rgba(255, 205, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(54, 162, 235, 0.2)'
+                ],
+                borderColor: [
+                  'rgb(255, 99, 132)',
+                  'rgb(255, 159, 64)',
+                  'rgb(255, 205, 86)',
+                  'rgb(75, 192, 192)',
+                  'rgb(54, 162, 235)'
+                ]
+            }
+          ]
+        };
+
+        console.log(data);
+
+        const config = {
+          type: 'bar',
+          data: data,
+          options: {
+            responsive: true,
+            plugins: {
+              legend: {
+                position: 'top',
+              },
+              title: {
+                display: false,
+                text: ''
+              }
+            }
+          }
+        };
+
+        var ctx = document.getElementById("chartTransactionFloationg01").getContext("2d");
+        var floatingChart = new Chart(ctx, config);
+    }
+}
+
 performancechart = {
 
     init: function() {
