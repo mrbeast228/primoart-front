8b8962946d9dd599f1bb524a263142b661567465
Maksim Demichev <mdemv@ya.ru>
2024-04-10 08:49:50 +0300

    Добавлен datatable для таблицы сервисов

--- apps/static/assets/js/primoart-datatables/primoart-datatables.js
+++ apps/static/assets/js/primoart-datatables/primoart-datatables.js
@@ -47,3 +47,31 @@ objectsProjectsTable = {
         });
     }
 }
+
+
+objectsServicesTable = {
+    init: function(tableId) {
+
+        var table = document.getElementById(tableId);
+        $(table).DataTable( {
+            "language": {
+                "decimal":        "",
+                "emptyTable":     "No data available in table",
+                "info":           "_START_-_END_ из _TOTAL_ сервисов",
+                "infoEmpty":      "Showing 0 to 0 of 0 entries",
+                "infoFiltered":   "(filtered from _MAX_ total entries)",
+                "infoPostFix":    "",
+                "thousands":      ",",
+                "lengthMenu":     "Показать _MENU_ сервисов",
+                "loadingRecords": "Loading...",
+                "processing":     "",
+                "search":         "Поиск:",
+                "zeroRecords":    "No matching records found",
+                "aria": {
+                    "orderable":  "Order by this column",
+                    "orderableReverse": "Reverse order this column"
+                }
+            }
+        });
+    }
+}
