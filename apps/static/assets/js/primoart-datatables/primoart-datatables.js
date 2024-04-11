1d9914236ef9006dfdeb94380e525d402372b1d0
Maksim Demichev <mdemv@ya.ru>
2024-04-11 14:55:42 +0300

    Datatable для транзакций

--- apps/static/assets/js/primoart-datatables/primoart-datatables.js
+++ apps/static/assets/js/primoart-datatables/primoart-datatables.js
@@ -102,3 +102,30 @@ objectsRobotsTable = {
         });
     }
 }
+
+objectsTransactionsTable = {
+    init: function(tableId) {
+
+        var table = document.getElementById(tableId);
+        $(table).DataTable( {
+            "language": {
+                "decimal":        "",
+                "emptyTable":     "No data available in table",
+                "info":           "_START_-_END_ из _TOTAL_ транзакций",
+                "infoEmpty":      "Showing 0 to 0 of 0 entries",
+                "infoFiltered":   "(filtered from _MAX_ total entries)",
+                "infoPostFix":    "",
+                "thousands":      ",",
+                "lengthMenu":     "Показать _MENU_ транзакций",
+                "loadingRecords": "Loading...",
+                "processing":     "",
+                "search":         "Поиск:",
+                "zeroRecords":    "No matching records found",
+                "aria": {
+                    "orderable":  "Order by this column",
+                    "orderableReverse": "Reverse order this column"
+                }
+            }
+        });
+    }
+}
